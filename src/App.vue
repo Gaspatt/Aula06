21d30444c608a463fea5e8b3c58b08de803daf7d
gaspatt <matheusgaspar29.if@gmail.com>
2024-03-22 11:56:05 -0300

    b

--- src/App.vue
+++ src/App.vue
@@ -13,54 +13,75 @@ const hobbies = ref('')
 const linguagens = ref('')
 const biografia = ref('')
 
-if (senha.value !== confirme.value) {
-    alert('As senhas não são iguais')
+
+
+function submit() {
+    if (document.querySelector('#senha').value !== document.querySelector('#confirme').value) {
+        alert('As senhas não são iguais')
+    }
+    else {
+        document.querySelector('.form').style.display = 'none'
+        document.querySelector('.resposta').style.display = 'block'
+        name.value = document.querySelector('#name').value
+        email.value = document.querySelector('#email').value
+        senha.value = document.querySelector('#senha').value
+        confirme.value = document.querySelector('#confirme').value
+        nascimento.value = document.querySelector('#nascimento').value
+        adress.value = document.querySelector('#adress').value
+        cidade.value = document.querySelector('#cidade').value
+        estado.value = document.querySelectorAll('#estado option')[document.querySelector('#estado').selectedIndex].text
+        hobbies.value = document.querySelector('#hobbies').value
+        linguagens.value = document.querySelector('#linguagens').value
+        biografia.value = document.querySelector('#biografia').value
+    }
 }
 
-function submit () {
-    document.querySelector('.form').style.display = 'none'
-    document.querySelector('.resposta').style.display = 'block'
-    name.value = document.querySelector('#name').value
-    email.value = document.querySelector('#email').value
-    senha.value = document.querySelector('#senha').value
-    confirme.value = document.querySelector('#confirme').value
-    nascimento.value = document.querySelector('#nascimento').value
-    adress.value = document.querySelector('#adress').value
-    cidade.value = document.querySelector('#cidade').value
-    estado.value = document.querySelector('#estado').value
-    hobbies.value = document.querySelector('#hobbies&shaw').value
-    linguagens.value = document.querySelector('#linguagens').value
-    biografia.value = document.querySelector('#biografia').value
+function voltar() {
+    document.querySelector('.form').style.display = 'flex'
+    document.querySelector('.resposta').style.display = 'none'
+    document.querySelector('#name').value = ''
+    document.querySelector('#email').value = ''
+    document.querySelector('#senha').value = ''
+    document.querySelector('#confirme').value = ''
+    document.querySelector('#nascimento').value = ''
+    document.querySelector('#adress').value = ''
+    document.querySelector('#cidade').value = ''
+    document.querySelector('#estado').value = ''
+    document.querySelector('#hobbies').value = ''
+    document.querySelector('#linguagens').value = ''
+    document.querySelector('#biografia').value = ''
 }
+
+
 
 </script>
 
 <template>
-    <div class="form">
+    <form class="form" @submit.prevent="submit">
         <label for="name">Informe seu nome:</label>
-        <input id="name" type="text">
+        <input id="name" type="text" required>
 
 
         <label for="email">Informe seu e-mail:</label>
-        <input id="email" type="email">
+        <input id="email" type="email" required>
 
         <label for="senha">Informe sua senha:</label>
-        <input id="senha" type="password">
+        <input id="senha" type="password" required>
 
         <label for="confirme">Confirme sua senha:</label>
-        <input id="confirme" type="password">
+        <input id="confirme" type="password" required>
 
         <label for="nascimento">Informe sua data de nascimento:</label>
-        <input id="nascimento" type="date">
+        <input id="nascimento" type="date" required>
 
         <label for="adress">Informe seu endereço:</label>
-        <input id="adress" type="text">
+        <input id="adress" type="text" required>
 
         <label for="cidade">Informe sua cidade:</label>
-        <input id="cidade" type="text">
+        <input id="cidade" type="text" required>
 
         <label for="estado">Informe seu estado:</label>
-        <select id="estado" name="estado">
+        <select id="estado" name="estado" required>
             <option value="AC">Acre</option>
             <option value="AL">Alagoas</option>
             <option value="AP">Amapá</option>
@@ -90,18 +111,18 @@ function submit () {
             <option value="TO">Tocantins</option>
         </select>
 
-        <label for="hobbies&shaw">Informe seu hobbie favorito</label>
-        <input id="hobbies&shaw" type="text">
+        <label for="hobbies">Informe seu hobbie favorito:</label>
+        <input id="hobbies" type="text" required>
 
         <label for="linguagens">Informe as linguagens de programacão que você domina:</label>
-        <input id="linguagens" type="text">
+        <input id="linguagens" type="text" required>
 
-        <label for="biografia">Escreva uma sua biografia</label>
-        <textarea name="" id="biografia" cols="50" rows="10" maxlength="1000" ></textarea>
+        <label for="biografia">Escreva uma sua biografia:</label>
+        <textarea name="biografia" id="biografia" cols="50" rows="10" maxlength="1000"></textarea>
 
 
-        <input id="submit" type="submit" value="Enviar" @click="submit">
-    </div>
+        <button type="submit">Enviar </button>
+    </form>
 
     <div class="resposta">
         <h1>Resposta</h1>
@@ -116,56 +137,81 @@ function submit () {
         <p>Hobbies: {{ hobbies }}</p>
         <p>Linguagens: {{ linguagens }}</p>
         <p>Biografia: {{ biografia }}</p>
+        <input id="voltar" type="submit" value="Voltar" @click="voltar">
     </div>
+
+
 
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
-    .form {
-        display: flex;
-        flex-direction: column; 
-        margin-top: 50px;
-    }
+.form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 50px;
+    width: 200%;
+}
 
-    input {
-        width: 300px;
-        height: 30px;
-        border-radius: 5px;
-        border: 1px solid #000;
-        margin-top: 2px;
-        margin-bottom: 10px;
+.poppins-light {
+    font-family: "Poppins", sans-serif;
+    font-weight: 300;
+    font-style: normal;
+}
 
-    }
+input {
+    width: 50%;
+    padding: 5px;
+    height: 30px;
+    margin-left: 25%;
+    border-radius: 5px;
+    border: 1px solid #000;
+    margin-top: 2px;
+    margin-bottom: 10px;
+    font-family: "Poppins", sans-serif;
+    font-weight: 5 00;
+    font-style: normal;
+    font-size: 1px;
 
-    select {
-        width: 300px;
-        height: 30px;
-        border-radius: 5px;
-        border: 1px solid #000;
-    }
+}
 
-    input[type="submit"] {
-        width: 300px;
-        height: 30px;
-        border-radius: 5px;
-        border: 1px solid #000;
-        background-color: #000;
-        color: #fff;
-    }
+label {
+    margin-left: 25%;
+    margin-top: 10px;
+}
 
-    input[type="submit"]:hover {
-        background-color: #888888;
-        color: #000;
-    }
+select {
+    width: 50%;
+    height: 30px;
+    margin-left: 25%;
+    border-radius: 5px;
+    border: 1px solid #000;
+}
 
-    textarea {
-        resize: none;
-    }
+button[type="submit"] {
+    width: 50%;
+    height: 30px;
+    margin-top: 3%;
+    margin-left: 25%;
+    border-radius: 5px;
+    border: 1px solid #000;
+    background-color: #000;
+    color: #fff;
+}
 
-    .resposta {
-        display: none;
-    }
-    
-    
+option,
+select {
+    background-color: purple;
+}
+
+textarea {
+    resize: none;
+    width: 50%;
+    margin-left: 25%
+}
+
+.resposta {
+    display: none;
+}
 </style>

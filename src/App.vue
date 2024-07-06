2b0474bdd31174d46b0b3254a0757f08a5a5ddf7
gaspatt <matheusgaspar29.if@gmail.com>
2024-07-05 22:52:46 -0300

    a

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
-np<script setup>
+np
+<script setup>
 import { ref } from 'vue'
 
 const name = ref('')
@@ -49,11 +50,24 @@ function submit() {
             hobbies.value += 'Lugares '
         }
 
+        linguagens.value = document.querySelectorAll('input[name="linguagens"]:checked')[0].value
 
-        hobbies.value = document.querySelector('#hobbies').value
-        linguagens.value = document.querySelector('#linguagens').value
         biografia.value = document.querySelector('#biografia').value
     }
+}
+
+function reset() {
+    document.querySelector('#name').value = ''
+    document.querySelector('#email').value = ''
+    document.querySelector('#senha').value = ''
+    document.querySelector('#confirme').value = ''
+    document.querySelector('#nascimento').value = ''
+    document.querySelector('#adress').value = ''
+    document.querySelector('#cidade').value = ''
+    document.querySelector('#estado').value = ''
+    document.querySelector('#hobbies').value = ''
+    document.querySelector('#linguagens').value = ''
+    document.querySelector('#biografia').value = ''
 }
 
 function voltar() {
@@ -79,99 +93,140 @@ function voltar() {
 <template>
     <form class="form" @submit.prevent="submit">
 
-        <div class="h1"><h1>Formulário de Cadastro</h1></div>
+        <div class="h1">
+            <h1>Formulário de Cadastro</h1>
+        </div>
         <div class="divmain divv">
-        <label for="name">Informe seu nome: <span class="ast">*</span></label>
-        <input id="name" type="text" required min="3" max="100"></div>
+            <label for="name">Informe seu nome: <span class="ast">*</span></label>
+            <input id="name" type="text" required min="3" max="100">
+        </div>
 
         <div class="divmain">
-        <label for="email">Informe seu e-mail: <span class="ast">*</span></label>
-        <input id="email" type="email" required></div>
+            <label for="email">Informe seu e-mail: <span class="ast">*</span></label>
+            <input id="email" type="email" required>
+        </div>
 
         <div class="divmain">
-        <label for="senha">Informe sua senha: <span class="ast">*</span></label>
-        <input id="senha" type="password" required></div>
+            <label for="senha">Informe sua senha: <span class="ast">*</span></label>
+            <input id="senha" type="password" required>
+        </div>
 
         <div class="divmain">
-        <label for="confirme">Confirme sua senha: <span class="ast">*</span></label>
-        <input id="confirme" type="password" required></div>
+            <label for="confirme">Confirme sua senha: <span class="ast">*</span></label>
+            <input id="confirme" type="password" required>
+        </div>
 
         <div class="divmain">
-        <label for="nascimento">Informe sua data de nascimento: <span class="ast">*</span></label>
-        <input id="nascimento" type="date"  min="1900-01-31" max="2024-12-31" required></div>
-        
-        <div class="divmain">
-        <label for="adress">Informe seu endereço: <span class="ast">*</span></label>
-        <input id="adress" type="text" required></div>
+            <label for="nascimento">Informe sua data de nascimento: <span class="ast">*</span></label>
+            <input id="nascimento" type="date" min="1900-01-31" max="2024-12-31" required>
+        </div>
 
         <div class="divmain">
-        <label for="cidade">Informe sua cidade: <span class="ast">*</span></label>
-        <input id="cidade" type="text" required></div>
+            <label for="adress">Informe seu endereço: <span class="ast">*</span></label>
+            <input id="adress" type="text" required>
+        </div>
 
         <div class="divmain">
-        <label for="estado">Informe seu estado: <span class="ast">*</span></label>
-        <select id="estado" name="estado" required>
-            <option value="AC">Acre</option>
-            <option value="AL">Alagoas</option>
-            <option value="AP">Amapá</option>
-            <option value="AM">Amazonas</option>
-            <option value="BA">Bahia</option>
-            <option value="CE">Ceará</option>
-            <option value="DF">Distrito Federal</option>
-            <option value="ES">Espírito Santo</option>
-            <option value="GO">Goiás</option>
-            <option value="MA">Maranhão</option>
-            <option value="MT">Mato Grosso</option>
-            <option value="MS">Mato Grosso do Sul</option>
-            <option value="MG">Minas Gerais</option>
-            <option value="PA">Pará</option>
-            <option value="PB">Paraíba</option>
-            <option value="PR">Paraná</option>
-            <option value="PE">Pernambuco</option>
-            <option value="PI">Piauí</option>
-            <option value="RJ">Rio de Janeiro</option>
-            <option value="RN">Rio Grande do Norte</option>
-            <option value="RS">Rio Grande do Sul</option>
-            <option value="RO">Rondônia</option>
-            <option value="RR">Roraima</option>
-            <option value="SC">Santa Catarina</option>
-            <option value="SP">São Paulo</option>
-            <option value="SE">Sergipe</option>
-            <option value="TO">Tocantins</option>
-        </select></div>
+            <label for="cidade">Informe sua cidade: <span class="ast">*</span></label>
+            <input id="cidade" type="text" required>
+        </div>
 
         <div class="divmain">
-        <label for="hobbies">Informe seu hobbie favorito: <span class="ast">*</span>
-        </label></div>
+            <label for="estado">Informe seu estado: <span class="ast">*</span></label>
+            <select id="estado" name="estado" required>
+                <option value="AC">Acre</option>
+                <option value="AL">Alagoas</option>
+                <option value="AP">Amapá</option>
+                <option value="AM">Amazonas</option>
+                <option value="BA">Bahia</option>
+                <option value="CE">Ceará</option>
+                <option value="DF">Distrito Federal</option>
+                <option value="ES">Espírito Santo</option>
+                <option value="GO">Goiás</option>
+                <option value="MA">Maranhão</option>
+                <option value="MT">Mato Grosso</option>
+                <option value="MS">Mato Grosso do Sul</option>
+                <option value="MG">Minas Gerais</option>
+                <option value="PA">Pará</option>
+                <option value="PB">Paraíba</option>
+                <option value="PR">Paraná</option>
+                <option value="PE">Pernambuco</option>
+                <option value="PI">Piauí</option>
+                <option value="RJ">Rio de Janeiro</option>
+                <option value="RN">Rio Grande do Norte</option>
+                <option value="RS">Rio Grande do Sul</option>
+                <option value="RO">Rondônia</option>
+                <option value="RR">Roraima</option>
+                <option value="SC">Santa Catarina</option>
+                <option value="SP">São Paulo</option>
+                <option value="SE">Sergipe</option>
+                <option value="TO">Tocantins</option>
+            </select>
+        </div>
 
         <div class="divmain">
-        <div class="checkbox">
-            <input id="filmes" type="checkbox" value="Filmes">
-            Filmes
-            <input id="livros" type="checkbox" value="Livros">
-            Livros
-            <input id="jogos" type="checkbox" value="Jogos">
-            Jogos
-            <input id="comidas" type="checkbox" value="Comidas">
-            Comidas
-            <input id="lugares" type="checkbox" value="Lugares">
-            Lugares
-        </div></div>
+            <label for="hobbies">Informe seu hobbie favorito: <span class="ast">*</span>
+            </label>
+        </div>
 
         <div class="divmain">
-        <label for="linguagens">Informe as linguagens de programacão que você domina: <span class="ast">*</span></label>
-        <input id="linguagens" type="text" v-model="linguagens" required></div>
+            <div class="checkbox">
+                <input type="checkbox" id="filmes" name="filmes" value="Filmes">
+                <label for="filmes">Filmes</label>
+
+                <input type="checkbox" id="livros" name="livros" value="Livros">
+                <label for="livros">Livros</label>
+
+                <input type="checkbox" id="jogos" name="jogos" value="Jogos">
+                <label for="jogos">Jogos</label>
+
+                <input type="checkbox" id="comidas" name="comidas" value="Comidas">
+                <label for="comidas">Comidas</label>
+
+                <input type="checkbox" id="lugares" name="lugares" value="Lugares">
+                <label for="lugares">Lugares</label>
+
+
+            </div>
+        </div>
 
         <div class="divmain">
-        <label for="biografia">Escreva uma sua biografia: <span class="ast">*</span></label>
-        <textarea name="biografia" id="biografia" cols="50" rows="10" maxlength="1000" v-model="biografia"></textarea></div>
+            <label for="linguagens">Informe as linguagens de programacão que você domina: <span
+                    class="ast">*</span></label>
+        </div>
+        <div class="divmain">
+            <div class="radio">
+                <input type="radio" id="js" name="linguagens" value="Java Script">
+                <label for="js">JavaScript</label>
 
+                <input type="radio" id="py" name="linguagens" value="Python">
+                <label for="py">Python</label>
 
-        <button type="submit">Enviar </button>
+                <input type="radio" id="java" name="linguagens" value="Java">
+                <label for="java">Java</label>
+
+                <input type="radio" id="c" name="linguagens" value="C">
+                <label for="c">C</label>
+            </div>
+        </div>
+
+        <div class="divmain">
+            <label for="biografia">Escreva uma sua biografia: <span class="ast">*</span></label>
+            <textarea name="biografia" id="biografia" cols="50" rows="10" maxlength="1000"
+                v-model="biografia"></textarea>
+        </div>
+
+<div class="botoes">
+    <button type="submit">Enviar</button>
+    <button type="reset" v-on="reset">Resetar</button>
+</div>
+ 
     </form>
 
     <div class="resposta">
-        <div class="h1"><h1>Informações do usuário</h1></div>
+        <div class="h1">
+            <h1>Informações do usuário</h1>
+        </div>
         <div class="pdiv">
             <div class="p">
                 <p>Nome: {{ name }}</p>
@@ -208,6 +263,7 @@ function voltar() {
             </div>
         </div>
         <input id="voltar" type="submit" value="Voltar" @click="voltar">
+        
     </div>
 
 
@@ -221,14 +277,31 @@ body {
     background-color: white;
 }
 
-.checkbox{
-   color: rgb(255, 255, 255);
-   width: 70%;
-   margin-left: 12%;
-   font-size: 17px;
-   display: flex;
-   align-items: center;
-   justify-content: center;
+.checkbox {
+    color: rgb(255, 255, 255);
+    width: 50%;
+    margin-left: 12%;
+    display: flex;
+    align-items: end;
+}
+
+.checkbox label {
+    color: rgb(255, 255, 255);
+    margin-left: 10px;
+
+}
+
+.radio {
+    color: rgb(255, 255, 255);
+    width: 50%;
+    margin-left: 12%;
+    display: flex;
+    align-items: end;
+}
+
+.radio label {
+    color: rgb(255, 255, 255);
+    margin-left: 10px;
 }
 
 .h1 {
@@ -241,9 +314,9 @@ body {
     margin-bottom: 1vh;
 }
 
- h1 {
+h1 {
     font-weight: bold;
- }
+}
 
 .form {
     display: flex;
@@ -317,16 +390,32 @@ select {
     justify-content: center;
 }
 
+.botoes {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin-bottom: 3%;
+    margin-top: 3%;
+}
 button[type="submit"] {
     width: 30%;
     height: 5vh;
-    margin-top: 3%;
-    margin-left: 35%;
     font-size: 20px;
-    margin-bottom: 3%;
     border-radius: 20px;
     border: 1px solid #000;
     background-color: #440e75;
+    color: #fff;
+    cursor: pointer;
+    margin-right: 5%
+}
+
+button[type="reset"] {
+    width: 30%;
+    height: 5vh;
+    font-size: 20px;
+    border-radius: 20px;
+    border: 1px solid #000;
+    background-color: #5c5275;
     color: #fff;
     cursor: pointer;
 }
@@ -363,7 +452,11 @@ textarea {
     margin-left: 0%;
 }
 
-.resposta input { 
+#rbio {
+    height: 10vh;
+}
+
+.resposta input {
     background-color: #440e75;
     color: white;
     height: 5vh;
@@ -386,7 +479,7 @@ textarea {
 
 .resposta h1 {
     font-weight: bold;
-    margin-bottom: 5%;
-    margin-top: 3%;
+    margin-bottom: 2.5%;
+    margin-top: 2%;
 }
 </style>
